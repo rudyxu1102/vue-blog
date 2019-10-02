a3f45ec4e3b841d679aed2840a0a45c971b4e0b6
xuhaodong <xuhaodong@51yund.com>
2019-10-02 17:16:56 +0800

    删除备案号

--- src/components/front/component/MyFooter.vue
+++ src/components/front/component/MyFooter.vue
@@ -3,7 +3,7 @@
         <a href="https://segmentfault.com/u/fatdong1" class="iconfont icon-iconsf"></a>
         <a class="iconfont icon-github" href="https://github.com/FatDong1"></a>
         <a href="http://www.facebook.com/fatdong1" class="iconfont icon-cion15"></a>
-        <p>粤ICP备17036548号</p>
+        <!-- <p>粤ICP备17036548号</p> -->
         <p>Copyright @ 2017 Powered by Vue. Designed by me | <router-link :to="{name: 'login'}" tag="span" class="login">站长登录</router-link></p>
     </div>
 </template>
